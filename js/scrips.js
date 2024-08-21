b42eb4e82ea8d5043140312177e578c041093208
Ever Pare <everpare91@gmail.com>
2024-08-21 17:26:21 -0300

    formularios

--- js/scrips.js
+++ js/scrips.js
@@ -1,1 +1,14 @@
-console.log("Página cargada");
+console.log("Página cargada");document.getElementById('registroForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var errorMensaje = document.getElementById('errorMensaje');
+
+    if (password !== confirmPassword) {
+        errorMensaje.textContent = "Las contraseñas no coinciden.";
+    } else {
+        errorMensaje.textContent = "";
+        alert("¡Registro exitoso!");
+    }
+});
